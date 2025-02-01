b3b6c7580def317da1f1224ca85592c40244367c
Silvia Islam <silviaislam.eera@gmail.com>
2025-02-01 21:49:32 +0600

    Formatted classname

--- components/ui/Pagination.jsx
+++ components/ui/Pagination.jsx
@@ -1,27 +1,49 @@
-import { HiChevronLeft, HiChevronRight } from "react-icons/hi"
-import Button from "@/components/ui/Button"
+// Icons
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
-const Pagination = ({ currentPage=1, totalPages=10, onPageChange=null }) => {
+// Components
+import Button from "@/components/ui/Button";
+
+const Pagination = ({
+  currentPage = 1,
+  totalPages = 10,
+  onPageChange = null,
+}) => {
   return (
-    <nav className="flex items-center justify-center space-x-4 mt-8">
-        <Button
-            disabled={currentPage === 1}
-        >
-            <HiChevronLeft className="w-5 h-5" />
-        </Button>
-      <span className="text-gray-700">
+    <nav
+      className={`
+        flex
+        items-center
+        justify-center
+        space-x-4 mt-8
+     `}
+    >
+      <Button disabled={currentPage === 1}>
+        <HiChevronLeft
+          className={`
+            w-5
+            h-5
+        `}
+        />
+      </Button>
+
+      <span
+        className={`
+            text-gray-700
+        `}
+      >
         {currentPage} / {totalPages}
       </span>
 
-        <Button
-            disabled={currentPage === totalPages}
-        >
-            <HiChevronRight className="w-5 h-5" />
-        </Button>
-
-
+      <Button disabled={currentPage === totalPages}>
+        <HiChevronRight className={`
+            w-5
+            h-5
+        `}
+        />
+      </Button>
     </nav>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
