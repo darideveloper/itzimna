d4ca317d8079c070feafc25858642542a940e307
daridev <darideveloper@gmail.com>
2025-02-05 12:19:12 -0600

    feat (Gallery): highlight active slide

--- components/layouts/Gallery.jsx
+++ components/layouts/Gallery.jsx
@@ -1,5 +1,8 @@
 "use client"
 
+// Libs
+import { useEffect, useState } from 'react'
+
 // Component:
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, A11y, Autoplay, Pagination } from 'swiper/modules'
@@ -11,6 +14,9 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
 export default function Gallery() {
+
+  const [slidesPerView, setSlidesPerView] = useState(3)
+  const [activeSlideIndex, setActiveSlideIndex] = useState(1)
 
   // Images keys 
   // (same file names without extension and with spaces replaced by underscores)
@@ -25,25 +31,40 @@ export default function Gallery() {
     "zendera"
   ]
 
+  useEffect(() => {
+    console.log({ slidesPerView, activeSlideIndex })
+  }, [slidesPerView, activeSlideIndex])
+
+  useEffect(() => {
+    // Get slides per view when resizing and when component mounts
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setSlidesPerView(1)
+      } else {
+        setSlidesPerView(3)
+      }
+    }
+    window.addEventListener('resize', handleResize)
+    handleResize()
+  }, [])
+
   return (
     <section
       className={`
         gallery
-        w-full
       `}
     >
       <Swiper
+        slidesPerView={slidesPerView}
         modules={[Navigation, A11y, Autoplay, Pagination]}
         navigation
         pagination={{ clickable: true }}
-        // Show active slide index
-        onSlideChange={(swiper) => console.log('slide change', swiper.activeIndex)}
         autoplay={{
-          delay: 8000,
+          delay: 3000,
           disableOnInteraction: false,
         }}
         breakpoints={{
-          230: {
+          320: {
             slidesPerView: 1,
             spaceBetween: 0,
           },
@@ -56,14 +77,33 @@ export default function Gallery() {
           !pb-12
           !pt-8
         `}
+        onSlideChange={(swiper) => {
+          setActiveSlideIndex(swiper.activeIndex + 1)
+        }}
       >
         {
           images.map((image, index) => (
-            <SwiperSlide 
+            <SwiperSlide
               key={index}
               className={`
+                relative
               `}
             >
+
+              <div
+                className={`
+                  overlay
+                  absolute
+                  w-full
+                  h-full
+                  bg-white
+                  duration-300
+                  z-10
+                  ${slidesPerView == 3 && activeSlideIndex != index ? "opacity-50" : "opacity-0"}
+                `}
+              >
+
+              </div>
               <Image
                 src={`/images/gallery/${image.replace(" ", "_")}.webp`}
                 alt={image}
@@ -77,7 +117,6 @@ export default function Gallery() {
             </SwiperSlide>
           ))
         }
-        <SwiperSlide>Slide 1</SwiperSlide>
       </Swiper>
     </section>
 
