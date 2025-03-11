242cd6e97f5be7a2cb9a6b0384a5a42d97ca9428
daridev <darideveloper@gmail.com>
2025-03-11 15:37:39 -0600

    feat (AOSInit): add aos to markdown titles

--- components/utils/AOSInit.jsx
+++ components/utils/AOSInit.jsx
@@ -7,11 +7,26 @@ import "aos/dist/aos.css"
 export default function AOSInit() {
   // Start AOS when component mounts
   useEffect(() => {
+
+    // Add aos to markdown h1, h2, h3, h4, h5, h6
+    const selectors = [
+      "h1", "h2", "h3", "h4", "h5", "h6", 
+    ]
+    const selector = selectors.map(selector => `.details ${selector}:not([data-aos])`).join(",")
+    const elements = document.querySelectorAll(selector)
+    if (elements.length) {
+      elements.forEach(element => {
+        element.setAttribute('data-aos', 'fade-down')
+      })
+    }
+    
+    // Start Aos
     AOS.init({
       duration: 1000,
       delay: window.innerWidth < 768 ? 0 : 100,
       once: true,
     })
+
   }, [])
 
   return null
