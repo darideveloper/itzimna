4872597d930d1cdca4cc8a7ef330487572a7e72f
daridev <darideveloper@gmail.com>
2025-04-03 13:06:42 -0600

    feat (middleware): home page in spanish

--- middleware.js
+++ middleware.js
@@ -3,7 +3,6 @@ import createMiddleware from 'next-intl/middleware'
 import { routing } from '@/i18n/routing'
 
 export default async function middleware(request) {
-
   const url = new URL(request.url)
   const pathname = url.pathname
   console.debug(`Incoming request for ${pathname}`)
@@ -11,21 +10,11 @@ export default async function middleware(request) {
   // Check if the request already has a locale in the URL
   const hasLocale = /^\/(es|en)(\/|$)/.test(pathname)
 
-  // Check for the `lang` cookie
-  const langCookie = request.cookies.get('NEXT_LOCALE')?.value
-
-  // If there's no locale in the URL and no `lang` cookie, redirect to the default locale
-  if (!hasLocale && !langCookie) {
-    console.debug('Locale missing and no lang cookie found, redirecting to default...')
-    url.pathname = `/${routing.defaultLocale}${pathname}`
-    return NextResponse.redirect(url)
-  }
-
-  // If the `lang` cookie exists but no locale is in the URL, redirect to the cookie's locale
-  if (!hasLocale && langCookie && routing.locales.includes(langCookie)) {
-    console.debug(`Locale missing, but lang cookie found: ${langCookie}. Redirecting...`)
-    url.pathname = `/${langCookie}${pathname}`
-    return NextResponse.redirect(url)
+  // If the request is for the root path `/`, serve it in Spanish without redirecting
+  if (pathname === '/') {
+    console.debug('Serving root path `/` in Spanish without redirect')
+    url.pathname = `/es`
+    return NextResponse.rewrite(url)
   }
 
   // Add internationalization middleware for other routes
