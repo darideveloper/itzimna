b4e5f09e314445c4386bb303c417f1871be1ef7c
Silvia <silviaislam.eera@gmail.com>
2025-06-24 19:28:52 +0600

    chore(Slider): empty silde removed

--- components/layouts/Gallery.jsx
+++ components/layouts/Gallery.jsx
@@ -1,19 +1,17 @@
-
 "use client"
 
 // Libs
-import { useTranslations } from 'next-intl'
-import { marked } from 'marked';
-import { getBestDevelopmentsImages } from '@/libs/api/best-developments-images'
-import { useState, useEffect } from 'react'
+import { useTranslations } from "next-intl"
+import { marked } from "marked"
+import { getBestDevelopmentsImages } from "@/libs/api/best-developments-images"
+import { useState, useEffect } from "react"
 
 // Components
-import Slider from '@/components/layouts/templates/Slider'
-import Spinner from '@/components/ui/Spinner'
+import Slider from "@/components/layouts/templates/Slider"
+import Spinner from "@/components/ui/Spinner"
 
 // Styles
 import "@/css/markdown.sass"
-
 
 /**
  * Gallery component to display a slider of images
@@ -21,49 +19,38 @@ import "@/css/markdown.sass"
  * @param {String} props.locale - Locale object for translations
  */
 export default function Gallery({ locale }) {
-
   // Get translations
-  const t = useTranslations('Home.GallerySection')
+  const t = useTranslations("Home.GallerySection")
   const [images, setImages] = useState([])
 
   // Load images from api when mounted or locale changes
   useEffect(() => {
-
     getBestDevelopmentsImages(locale).then((data) => {
-
       // Format images data
       const imagesData = data.map((image) => {
         return {
           url: image.image,
           alt: image.alt_text,
-          name: image.image.split('/').pop().split('.')[0] // Extract name from URL
+          name: image.image.split("/").pop().split(".")[0], // Extract name from URL
         }
       })
-
-      ///Add white images at the beginning and end
-      const whiteImage = {
-        url: '/images/gallery/white.webp',
-        alt: t('imagesPre.development'),
-        name: 'white'
-      }
-      imagesData.unshift(whiteImage)
-      imagesData.push(whiteImage)
-
       // Update state with images data
       setImages(imagesData)
     })
   }, [locale])
 
   return (
-    <section
-      className={`gallery-wrapper relative`}
-    >
-      <Spinner isLoading={images.length === 0} transparentModal={true} className='mt-20 !items-end' />
+    <section className={`gallery-wrapper relative`}>
+      <Spinner
+        isLoading={images.length === 0}
+        transparentModal={true}
+        className="mt-20 !items-end"
+      />
       <Slider
         id="gallery"
         imagesData={images}
-        title={t('title')}
-        descriptionMd={marked(t('description'))}
+        title={t("title")}
+        descriptionMd={marked(t("description"))}
       />
     </section>
   )
