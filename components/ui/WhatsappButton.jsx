3b0ea91858d3a3df2a33f5900f29670f13c0ece8
daridev <darideveloper@gmail.com>
2025-07-11 15:46:43 -0600

    refactor (WhatsappButton): use clsx

--- components/ui/WhatsappButton.jsx
+++ components/ui/WhatsappButton.jsx
@@ -6,6 +6,7 @@ import Link from "next/link"
 
 // Libs
 import { useTranslations } from "next-intl"
+import clsx from "clsx"
 
 
 export default function WhatsappButton() {
@@ -22,24 +23,24 @@ export default function WhatsappButton() {
     <Link
       href={whatsappUrl}
       target="_blank"
-      className={`
-        whatsapp-button
-        fixed
-        bottom-4
-        right-4
-        z-50
-        bg-whatsapp
-        text-white
-        p-3
-        rounded-full
-        shadow-sm
-        shadow-white/50
-        duration-300
-        scale-[130%]
-        hover:scale-110
-      `}
+      className={clsx(
+        "whatsapp-button",
+        "fixed",
+        "bottom-4",
+        "right-4",
+        "z-50",
+        "bg-whatsapp",
+        "text-white",
+        "p-3",
+        "rounded-full",
+        "shadow-sm",
+        "shadow-white/50",
+        "duration-300",
+        "scale-[130%]",
+        "hover:scale-110"
+      )}
     >
-      <FaWhatsapp className="text-3xl lg:text-4xl" />
+      <FaWhatsapp className={clsx("text-3xl", "lg:text-4xl")} />
     </Link>
   )
 }
